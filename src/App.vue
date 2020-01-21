6aaa34849f81c6a23c32519b3472382e438104cf
Harry <harpreetgoraya95@gmail.com>
2020-01-21 14:12:20 -0500

    Inital divs

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,23 @@ export default {
 </script>
 
 <style>
-#app {
+#app-div {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+body, html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  min-height: 100vh;
+  background: #2c3e50;
+  overflow: hidden;
+}
+
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="main-div">
+    <div id="temp-div"></div>
+    <div id="more-info-div"></div>
     <h1>HEEYYY </h1>
+    
   </div>
 </template>
 
@@ -16,9 +19,47 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-h1 {
-  color: var(--BLUEGREEN);
+
+.main-div{
+  display: inline-block;
+  background: grey;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("../assets/images/clouds1.jpg");
+  background-size: cover;
+  background-attachment: inherit;
 }
+
+
+#temp-div{
+  display: inline-block;
+  width: 70%;
+  height: 100vh;
+}
+
+#more-info-div{
+  display: inline-block;
+  width: 30%;
+  height: 100vh;
+  background: inherit;
+
+
+}
+
+#main-info-div  ::before{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  box-shadow: inset 0 0 2000px rgba(0, 0, 0, 0.5);
+
+
+
+
+}
+
+
 
 
 </style>
