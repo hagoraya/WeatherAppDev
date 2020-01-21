0a41a40a46f3d4d88054059b42dc5b715195208f
Harry <harpreetgoraya95@gmail.com>
2020-01-21 11:59:05 -0500

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-div">
+    <h1>HEEYYY </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h1 {
+  color: var(--BLUEGREEN);
+}
+
+
+</style>
