c62275f184adab9a21f6f738d0c47a625e101b75
Harry <harpreetgoraya95@gmail.com>
2020-01-21 14:26:44 -0500

    transparent back ground for info

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -27,7 +27,6 @@ export default {
   width: 100vw;
   background-image: url("../assets/images/clouds1.jpg");
   background-size: cover;
-  background-attachment: inherit;
 }
 
 
@@ -41,23 +40,15 @@ export default {
   display: inline-block;
   width: 30%;
   height: 100vh;
-  background: inherit;
-
-
-}
-
-#main-info-div  ::before{
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  box-shadow: inset 0 0 2000px rgba(0, 0, 0, 0.5);
-
+  background-color: rgba(0, 0, 0, .35);  
+  backdrop-filter: blur(5px);
+  box-shadow: black;
 
 
 
 }
+
+
 
 
 
